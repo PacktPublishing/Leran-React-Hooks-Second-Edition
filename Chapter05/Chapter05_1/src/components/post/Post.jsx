5d1ce94e0f54074c8e5314838f1be5a9451c2cec
Daniel Bugl <me@omnidan.net>
2024-10-09 18:50:15 +0200

    finalize chapter 5_1

--- Chapter05/Chapter05_1/src/components/post/Post.jsx
+++ Chapter05/Chapter05_1/src/components/post/Post.jsx
@@ -1,9 +1,12 @@
 import { PropTypes } from 'prop-types'
+import { useContext } from 'react'
+import { ThemeContext } from '@/contexts/ThemeContext.js'
 
 export function Post({ title, content, author }) {
+  const theme = useContext(ThemeContext)
   return (
     <div>
-      <h3>{title}</h3>
+      <h3 style={{ color: theme.primaryColor }}>{title}</h3>
       <div>{content}</div>
       <br />
       <i>
