d503fffbe1f43b7cf5d991e16421f17d7953cb0e
Daniel Bugl <me@omnidan.net>
2024-11-22 12:00:17 +0100

    add chapter 7/1 code

--- Chapter07/Chapter07_1/src/components/post/CreatePost.jsx
+++ Chapter07/Chapter07_1/src/components/post/CreatePost.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useActionState } from 'react'
 import { useMutation } from '@tanstack/react-query'
 import { createPost, queryClient } from '@/api.js'
 import { UserContext } from '@/contexts/UserContext.js'
@@ -13,18 +13,21 @@ export function CreatePost() {
     },
   })
 
-  async function handleSubmit(e) {
-    e.preventDefault()
-
-    const title = e.target.elements.title.value
-    const content = e.target.elements.content.value
-    const post = { title, content, author: username, featured: false }
-
-    createPostMutation.mutate(post, { onSuccess: () => e.target.reset() })
-  }
+  const [error, submitAction, isPending] = useActionState(
+    async (currentState, formData) => {
+      const title = formData.get('title')
+      const content = formData.get('content')
+      const post = { title, content, author: username, featured: false }
+      try {
+        await createPostMutation.mutateAsync(post)
+      } catch (err) {
+        return err
+      }
+    },
+  )
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form action={submitAction}>
       <div>
         Author: <b>{username}</b>
       </div>
@@ -33,16 +36,8 @@ export function CreatePost() {
         <input type='text' name='title' id='create-title' />
       </div>
       <textarea name='content' />
-      <input
-        type='submit'
-        value='Create'
-        disabled={createPostMutation.isPending}
-      />
-      {createPostMutation.isError && (
-        <div style={{ color: 'red' }}>
-          {createPostMutation.error.toString()}
-        </div>
-      )}
+      <input type='submit' value='Create' disabled={isPending} />
+      {error && <div style={{ color: 'red' }}>{error.toString()}</div>}
     </form>
   )
 }
