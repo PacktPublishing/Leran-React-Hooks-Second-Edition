32200aa63a675161892b7f5f3b999290c32ae157
Daniel Bugl <me@omnidan.net>
2025-02-07 10:15:12 +0100

    add OnlineIndicator example

--- Chapter09/Chapter09_1/src/pages/Demo.jsx
+++ Chapter09/Chapter09_1/src/pages/Demo.jsx
@@ -3,6 +3,7 @@ import { InitialWidthMeasure } from '@/components/demo/useRef/InitialWidthMeasur
 import { Timer } from '@/components/demo/useRef/Timer.jsx'
 import { HighlightFocus } from '@/components/demo/useImperativeHandle/HighlightFocus.jsx'
 import { AriaInput } from '@/components/demo/useId/AriaInput.jsx'
+import { OnlineIndicator } from '@/components/demo/useSyncExternalStore/OnlineIndicator.jsx'
 
 export function Demo() {
   return (
@@ -12,8 +13,12 @@ export function Demo() {
       <AutoFocus />
       <InitialWidthMeasure />
       <Timer />
+      <h2>useImperativeHandle</h2>
       <HighlightFocus />
+      <h2>useId</h2>
       <AriaInput />
+      <h2>useSyncExternalStore</h2>
+      <OnlineIndicator />
     </div>
   )
 }
