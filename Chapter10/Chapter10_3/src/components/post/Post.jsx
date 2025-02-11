3a141814b604698fc8c9bd0bc1383d87993942e4
Daniel Bugl <me@omnidan.net>
2025-02-11 16:55:15 +0100

    init chapter 10/3

---
+++ Chapter10/Chapter10_3/src/components/post/Post.jsx
@@ -0,0 +1,29 @@
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { useContext } from 'react'
+import { ThemeContext } from '@/contexts/ThemeContext.js'
+import { CommentSection } from '@/components/comment/CommentSection.jsx'
+import { fetchPost } from '@/api.js'
+
+export function Post({ id }) {
+  const { data } = useSuspenseQuery({
+    queryKey: ['post', id],
+    queryFn: async () => await fetchPost({ id }),
+  })
+  const { title, content, author } = data
+
+  const theme = useContext(ThemeContext)
+
+  return (
+    <div>
+      <h3 style={{ color: theme.primaryColor }}>{title}</h3>
+      <div>{content}</div>
+      <br />
+      <i>
+        Written by <b>{author}</b>
+      </i>
+      <br />
+      <br />
+      <CommentSection />
+    </div>
+  )
+}
